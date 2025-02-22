4e1cedfa7bcaba8187013403240bf15e0b64efd4
Tirasha <122605216+Tirasha@users.noreply.github.com>
2025-02-22 07:20:46 +0530

    initial commit

---
+++ src/main/java/com/example/Emp_management_Sys_backend/EmpManagementSysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Emp_management_Sys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagementSysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpManagementSysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Emp_management_Sys_backend/EmpManagementSysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Emp_management_Sys_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementSysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
